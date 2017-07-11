36b6332e2026e5c0936bdb5c3801dbef54abaa68
marton.perei <mindig.marton@gmail.com>
2017-07-11 23:34:13 +0200

    Initial commit

---
+++ example.py
@@ -0,0 +1,17 @@
+if __name__ == "__main__":
+    import nadtcp
+    import logging
+    import asyncio
+
+    logging.basicConfig(level=logging.DEBUG)
+
+    _LOGGER = logging.getLogger(__name__)
+
+    if __name__ == "__main__":
+        loop = asyncio.get_event_loop()
+
+        nad_client = nadtcp.NADC338Protocol.create_nad_connection(loop=loop, target_ip='192.168.1.112')
+
+        loop.run_until_complete(nad_client)
+
+        loop.run_forever()

85a5be8e3afe19313cef5f13c1dc4469789eaf32
Marton Perei <mindig.marton@gmail.com>
2019-03-13 19:45:09 +0100

    Version 0.2.0.dev0

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(name='nadtcp',
-      version='0.1.1',
+      version='0.2.0.dev0',
       description='Library for TCP/IP enabled NAD amplifiers. Currently only the C338 is supported.',
       url='https://gitlab.com/mindig.marton/nadtcp',
-      download_url='https://gitlab.com/mindig.marton/nadtcp/repository/archive.zip?ref=0.1.1',
+      download_url='https://gitlab.com/mindig.marton/nadtcp/repository/archive.zip?ref=0.2.0.dev0',
       author='mindigmarton',
       license='MIT',
       packages=['nadtcp'],
